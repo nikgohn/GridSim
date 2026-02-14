c17cc54b30ce6f60a32cd461bc545d1f51e4b8d5
Nikgohn <46885334+nikgohn@users.noreply.github.com>
2026-02-14 22:28:10 +0300

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gridsim.app',
+  appName: 'GridSim',
+  webDir: 'out'
+};
+
+export default config;
